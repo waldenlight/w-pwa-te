21453bf1589c9fc85ec317d96f066cbf2ad4a110
waldenlight <76961678+waldenlight@users.noreply.github.com>
2023-04-24 08:01:03 -0600

    Edit frontend JS

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,38 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('text', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('text')) {
+        console.log('text database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('text', { keyPath: 'id', autoIncrement: true });
+      console.log('text database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const textDb = await openDB('text', 1);
+  const tx = textDb.transaction('text', 'readwrite');
+  const store = tx.objectStore('text');
+  const request = store.put({ id: id, text: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+  const textDb = await openDB('text', 1);
+  const tx = textDb.transaction('text', 'readonly');
+  const store = tx.objectStore('text');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
