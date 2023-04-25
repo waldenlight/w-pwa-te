bf15d191c16ad4abb178d6d62952c83f4b049971
waldenlight <76961678+waldenlight@users.noreply.github.com>
2023-04-25 08:50:39 -0600

    Fix service worker

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,7 +7,7 @@ const initdb = async () =>
         console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('jate', { keyPath: 'content', autoIncrement: true });
       console.log('jate database created');
     },
   });
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, jate: content });
+  const request = store.put({ jate: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
